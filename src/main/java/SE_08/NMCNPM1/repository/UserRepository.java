9d431bcc7bf843eef1f5a904679708a149cbb35e
ThanhIT0410 <trinhthanhit104@gmail.com>
2024-10-16 13:32:25 +0700

    Cập nhật tính năng xác thực người dùng bằng Spring security, với danh sách người dùng lấy từ database trên AWS

---
+++ src/main/java/SE_08/NMCNPM1/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package SE_08.NMCNPM1.repository;
+
+import SE_08.NMCNPM1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
