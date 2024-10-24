79ad567dedd4d7b8fbc36db1797ffc8950f8ef8f
ThanhIT0410 <trinhthanhit104@gmail.com>
2024-10-25 03:26:12 +0700

    Sửa giao diện

--- src/main/resources/static/js/menu-toggle.js
+++ src/main/resources/static/js/menu-toggle.js
@@ -5,13 +5,37 @@ if (localStorage.getItem('sidebarState') === 'active') {
     sidebar.classList.add('active');
 }
 
-btn.onclick = function() {
-    sidebar.classList.toggle('active');
+let canToggle = true;
+let sidebarReactivate = 0;
 
-    if (sidebar.classList.contains('active')) {
-        localStorage.setItem('sidebarState', 'active');
-    }
-    else {
+function checkViewportWidth() {
+    if (window.innerWidth < 800) {
+        sidebar.classList.remove('active');
         localStorage.setItem('sidebarState', 'inactive');
+        sidebarReactivate = 1;
+        canToggle = false;
+    } else {
+        if (sidebarReactivate === 1) {
+            sidebar.classList.add('active');
+            localStorage.setItem('sidebarState', 'active');
+            sidebarReactivate = 0;
+        }
+        canToggle = true;
     }
-} ;
+}
+
+btn.onclick = function() {
+    if (canToggle) {
+        sidebar.classList.toggle('active');
+
+        if (sidebar.classList.contains('active')) {
+            localStorage.setItem('sidebarState', 'active');
+        } else {
+            localStorage.setItem('sidebarState', 'inactive');
+        }
+    }
+};
+
+window.addEventListener('resize', checkViewportWidth);
+
+checkViewportWidth();
