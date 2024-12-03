ab64073b1cfef064a9c233e708b3f1cfbf9b5b6a
ThanhIT0410 <trinhthanhit104@gmail.com>
2024-12-04 05:04:45 +0700

    API báo + sửa giao diện homepage

--- src/main/java/SE_08/NMCNPM1/controller/HomepageController.java
+++ src/main/java/SE_08/NMCNPM1/controller/HomepageController.java
@@ -1,5 +1,6 @@
 package SE_08.NMCNPM1.controller;
 
+import SE_08.NMCNPM1.service.NewsService;
 import SE_08.NMCNPM1.service.WeatherService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +9,21 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomepageController {
+
     private final WeatherService weatherService;
+    private final NewsService newsService;
 
     @Autowired
-    public HomepageController(WeatherService weatherService) {
+    public HomepageController(WeatherService weatherService, NewsService newsService) {
         this.weatherService = weatherService;
+        this.newsService = newsService;
     }
 
     @GetMapping("/homepage")
@@ -23,7 +31,6 @@ public class HomepageController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
         request.getSession().setAttribute("username", username);
-        model.addAttribute("username", username);
 
         model.addAttribute("temperature", weatherService.getTemperature());
         model.addAttribute("description", weatherService.getDescription());
@@ -31,6 +38,15 @@ public class HomepageController {
         model.addAttribute("backgroundColor", weatherService.getBackgroundColor());
         model.addAttribute("icon", weatherService.getIcon());
 
+        List<Map<String, String>> articles = newsService.getArticles();
+        model.addAttribute("articles", articles);
+
         return "homepage";
+    }
+
+    @GetMapping("/homepage/news")
+    public String getHomepageNews(@RequestParam String url, Model model) {
+        model.addAttribute("url", url);
+        return "homepage-news";
     }
 }
