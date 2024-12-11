3e061820eb9e6fa390329604a6e6b64a4d41a768
ThanhIT0410 <trinhthanhit104@gmail.com>
2024-12-11 13:27:59 +0700

    Ver 1 final fix

--- src/main/java/SE_08/NMCNPM1/repository/KhoanThuRepository.java
+++ src/main/java/SE_08/NMCNPM1/repository/KhoanThuRepository.java
@@ -3,19 +3,20 @@ package SE_08.NMCNPM1.repository;
 import SE_08.NMCNPM1.model.Khoanthu;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface KhoanThuRepository extends PagingAndSortingRepository<Khoanthu, Integer> {
     @Query("SELECT k FROM Khoanthu k WHERE CONCAT(COALESCE(k.id, ''), ' ', COALESCE(k.tenkhoanthu, ''), ' ', COALESCE(k.sotien, ''), ' ', COALESCE(k.ngaytao, ''), ' ', COALESCE(k.hanchot, ''), ' ', COALESCE(k.batbuoc, ''), ' ', COALESCE(k.nguoitao, '')) LIKE %?1%")
-
     Page<Khoanthu> search(@Param("keyword") String keyword, Pageable pageable);
 
     Optional<Khoanthu> findById(int id);
@@ -23,5 +24,10 @@ public interface KhoanThuRepository extends PagingAndSortingRepository<Khoanthu,
     void save(Khoanthu khoanthu);
 
     void deleteById(int id);
+
+    List<Khoanthu> findTop5ByHanchotGreaterThanOrderByHanchotAsc(LocalDateTime currentDate);
+
+    List<Khoanthu> findAll(Sort sort);
+
 }
 
